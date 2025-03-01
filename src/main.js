dbdd828c54ac01c0027d8186bd32f7794d245200
Abdulkareem Abdulmateen <162720141+karmat-1@users.noreply.github.com>
2025-03-01 17:45:31 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// run one task at a time by uncommenting it and commenting the rest
-
 // import deleteTask from "./commands/delete-task.command.js";
 
 // deleteTask();
@@ -8,6 +6,44 @@
 
 // readTask();
 
+// import addTask from "./commands/add-task.command.js";
+
+// addTask();
+
+import readline from "readline";
+import deleteTask from "./commands/delete-task.command.js";
+import readTask from "./commands/read-task.command.js";
 import addTask from "./commands/add-task.command.js";
 
-addTask();
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function showMenu() {
+  console.log("\nChoose a task to perform:");
+  console.log("1. Add Task");
+  console.log("2. Read Task");
+  console.log("3. Delete Task");
+ 
+
+  rl.question("Enter the number of the task: ", (answer) => {
+    switch (answer.trim()) {
+      case "1":
+        addTask();
+        break;
+      case "2":
+        readTask();
+        break;
+      case "3":
+        deleteTask();
+        break;
+      default:
+        console.log("Invalid option. Please choose 1, 2, 3");
+        showMenu();
+    }
+  });
+}
+
+showMenu();
+
